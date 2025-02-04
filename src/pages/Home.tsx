9045b20c5395606fa1205a8e3a052d8fbe0b38a5
eshanized <m.eshanized@gmail.com>
2025-02-04 13:59:11 +0530

    style: add ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import { Github, Gitlab as GitlabLogo, ArrowRight, Code, Briefcase, BarChart as ChartBar, Twitter, Linkedin, Instagram, Youtube, Facebook } from 'lucide-react';
 import { Link } from 'react-router-dom';
