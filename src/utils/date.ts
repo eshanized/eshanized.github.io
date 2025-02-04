63e218b4099b177d1adaf63f30e633743ebdb002
eshanized <m.eshanized@gmail.com>
2025-02-04 12:59:10 +0530

    chore: bump lines

--- src/utils/date.ts
+++
@@ -1,9 +0,0 @@
-import { format, formatDistanceToNow } from 'date-fns';
-
-export function formatDate(date: string): string {
-  return format(new Date(date), 'MMM dd, yyyy');
-}
-
-export function formatRelativeDate(date: string): string {
-  return formatDistanceToNow(new Date(date), { addSuffix: true });
-}
