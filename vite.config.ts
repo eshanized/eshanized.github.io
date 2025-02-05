720e988cbb5951e757d7784772847e6646b54f10
eshanized <m.eshanized@gmail.com>
2025-02-05 19:52:57 +0530

    chore: update for gh

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import VitePluginSitemap from 'vite-plugin-sitemap';
 
-// Define your website URLs
-const githubUrl = 'https://eshanized.github.io';
-const gitlabUrl = 'https://eshanized.gitlab.io';
-
-// Use the appropriate URL based on the deployment target
-const siteUrl = process.env.DEPLOY_TARGET === 'gitlab' ? gitlabUrl : githubUrl;
+// Define website URL
+const siteUrl = 'https://eshanized.is-a.dev';
 
 // Define your routes
 const routes = [
@@ -36,7 +32,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: 'scripts.js',  // JavaScript output as scripts.js
+        entryFileNames: 'scripts.js',
         chunkFileNames: 'scripts.js',
         assetFileNames: (assetInfo) => {
           if (assetInfo.name === 'style.css') {
