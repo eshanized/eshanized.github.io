10351658c5a0015872f86cb425245782760d20cf
eshanized <m.eshanized@gmail.com>
2025-02-06 15:09:18 +0530

    🚧 WIP: creating pipelines

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import VitePluginSitemap from 'vite-plugin-sitemap';
 
-// Define your website URLs
-const githubUrl = 'https://eshanized.github.io';
-const gitlabUrl = 'https://eshanized.gitlab.io';
-
-// Use the appropriate URL based on the deployment target
-const siteUrl = process.env.DEPLOY_TARGET === 'gitlab' ? gitlabUrl : githubUrl;
+// Define your website URL
+const siteUrl = 'https://eshanized.is-a.dev';
 
 // Define your routes
 const routes = [
@@ -36,16 +32,13 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: 'scripts.js',  // JavaScript output as scripts.js
-        chunkFileNames: 'scripts.js',
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'style.css') {
-            return 'style.css';
+          if (assetInfo.name === 'index.css') {
+            return 'assets/[name].[hash][extname]';
           }
-          if (assetInfo.name === 'robots.txt') {
-            return 'robots.txt';
-          }
-          return '[name][extname]';
+          return 'assets/[name].[hash][extname]';
         }
       }
     }
